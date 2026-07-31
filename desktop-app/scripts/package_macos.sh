334bacbfede753afe5e43ec41e92502058caa331
Andrew Pareles <andrewpareles@gmail.com>
2026-07-30 23:26:29 -0400

    Use checked-in macOS ICNS icon

--- desktop-app/scripts/package_macos.sh
+++ desktop-app/scripts/package_macos.sh
@@ -5,5 +5,4 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$REPO_ROOT"
 
 npm run build
-npm run icons:mac
 npx electron-builder --config electron-builder.config.cjs --mac "$@"
